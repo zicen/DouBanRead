6bc753924d33cfc8b643442e4cd186ecf984a281
zicen <1140377034@qq.com>
2018-01-03 18:24:17 +0800

    toolbar

--- app/src/main/java/com/zhenquan/doubanread/ui/BookListActivity.kt
+++ app/src/main/java/com/zhenquan/doubanread/ui/BookListActivity.kt
@@ -2,24 +2,36 @@ package com.zhenquan.doubanread.ui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
 import android.util.Log
+import android.view.MenuItem
 import android.view.View
 import com.zhenquan.doubanread.R
 import com.zhenquan.doubanread.base.BaseActivity
+import org.jetbrains.anko.find
 
 class BookListActivity : BaseActivity() {
     override fun initView(rootView: View?) {
 
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        val title = intent.extras["title"]
-        Log.e("111", "title:" + title)
-    }
+    val toolbar: Toolbar by lazy { find<Toolbar>(R.id.toolbar) }
 
+    override fun initListener() {
+        super.initListener()
+        val title = intent.extras.getString("title")
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = title
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
     override fun getLayoutId(): Int {
         return R.layout.activity_book_list
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> finish()
+        }
+        return super.onOptionsItemSelected(item)
+    }
 }
