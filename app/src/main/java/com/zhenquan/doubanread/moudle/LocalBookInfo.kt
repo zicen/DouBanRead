5f2bcc863a955afa70c25394495f24d9c52e733e
zicen <1140377034@qq.com>
2018-01-10 10:28:25 +0800

    本地书架完成,UI后期再改

---
+++ app/src/main/java/com/zhenquan/doubanread/moudle/LocalBookInfo.kt
@@ -0,0 +1,9 @@
+package com.zhenquan.doubanread.moudle
+
+/**
+ * Created by ry on 2018/1/10.
+ */
+data class LocalBookInfo(
+        val name:String,
+        val image:String
+)
--- app/src/main/java/com/zhenquan/doubanread/ui/local/LocalFragment.kt
+++ app/src/main/java/com/zhenquan/doubanread/ui/local/LocalFragment.kt
@@ -1,20 +1,55 @@
 package com.zhenquan.doubanread.ui.local
 
+import android.content.Intent
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.View
+import com.squareup.picasso.Picasso
 import com.zhenquan.doubanread.R
 import com.zhenquan.doubanread.base.BaseFragment
+import com.zhenquan.doubanread.base.adapter.BaseRecyclerAdapter
+import com.zhenquan.doubanread.base.adapter.SmartViewHolder
+import com.zhenquan.doubanread.moudle.Book
+import com.zhenquan.doubanread.moudle.LocalBookInfo
+import com.zhenquan.doubanread.ui.classfiy.BookDetailActivity
+import com.zhenquan.doubanread.ui.classfiy.BookTagListActivity
+import com.zhenquan.doubanread.ui.classfiy.ClassfiyAdapter
+import org.jetbrains.anko.find
+import java.lang.reflect.Array
 
 /**
  * Created by 44162 on 2018/1/3.
  * @描述 本地
  */
 class LocalFragment : BaseFragment() {
-
+    val adapter: BaseRecyclerAdapter<LocalBookInfo> by lazy {
+        object : BaseRecyclerAdapter<LocalBookInfo>(R.layout.item_local_book_list) {
+            override fun onBindViewHolder(holder: SmartViewHolder, model: LocalBookInfo, position: Int) {
+                holder.text(R.id.iv_local_book_list, model.name)
+                holder.itemView.setOnClickListener {
+//                    val intent = Intent()
+//                    intent.setClass(holder.itemView.context, BookDetailActivity::class.java)
+//                    intent.putExtra("title", model.name)
+//                    startActivity(intent)
+                }
+            }
+        }
+    }
     override fun getLayoutId(): Int {
         return R.layout.fragment_local
     }
 
     override fun initView(rootView: View?) {
-
+        val fragment_local_recycle = rootView?.find<RecyclerView>(R.id.fragment_local_recycle)
+        fragment_local_recycle?.layoutManager = GridLayoutManager(activity, 3)
+        fragment_local_recycle?.adapter = adapter
+        var  list = ArrayList<LocalBookInfo>()
+        var num = 0
+        while (num <20){
+            list.add(LocalBookInfo("bookname" + num,""))
+            num++
+        }
+        adapter.refresh(list)
     }
 }
